2838d614a49fbc768d9a0ad583450309d7b34e39
MikkelHojbjerg <89967176+MikkelHojbjerg@users.noreply.github.com>
2023-03-25 14:56:55 +0100

    njord er blevet fyret

--- freyr.fsx
+++ freyr.fsx
@@ -7,19 +7,18 @@ open FSharp.Data
 #load "valhalla.fsx"
 open Valhalla
 
-
 let fetchFromA (area: string) =
-    let url = njord.urlA + (area.Replace (" ", "%20"))
-    Http.RequestString($"{url}", httpMethod = "POST", headers = [ "Authorization", $"Bearer {njord.token}" ])
+    let url = heimdal.url_A + (area.Replace (" ", "%20"))
+    Http.RequestString($"{url}", httpMethod = "POST", headers = [ "Authorization", $"Bearer {heimdal.token}" ])
 
 let sendToB (data: string) =
-    let response = Http.RequestString($"{njord.urlB}", httpMethod = "POST", body = TextRequest data, headers = [ "Authorization", $"Bearer {njord.token}" ])
+    let response = Http.RequestString($"{heimdal.url_B}", httpMethod = "POST", body = TextRequest data, headers = [ "Authorization", $"Bearer {heimdal.token}" ])
     printfn $"Response from B: {response}"
 
 
 let mutable lastJson = ""
 let run () =
-        for area in njord.areas do
+        for area in heimdal.areas do
             let response = fetchFromA area
             let json = (thor.toJson (response, area))
             if json <> lastJson then
