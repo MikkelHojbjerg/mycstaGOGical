d8e054716c9fff3381e4cfdd09fc261e23a4f10f
MikkelHojbjerg <89967176+MikkelHojbjerg@users.noreply.github.com>
2023-03-25 17:49:13 +0100

    test query

--- freyr.fsx
+++ freyr.fsx
@@ -6,7 +6,6 @@ open System
 open System.IO
 open FSharp
 open FSharp.Data
-
 #load "valhalla.fsx"
 open Valhalla
 
@@ -16,6 +15,11 @@ let fetchFromA (area: string) =
     printfn $"Response from A: {response}"
     response
 
+//let fetchFromATime (area: string) =
+//    let url = heimdall.urlA + (area.Replace (" ", "%20")) + ("&time=" + currTime.timeNow)
+//    let response = Http.RequestString($"{url}", httpMethod = "POST", headers = [ "Authorization", $"Bearer {heimdall.token}" ])
+//    printfn $"Response from A: {response}"
+//    response
 
 let sendToB (data: string) =
     let response = Http.RequestString($"{heimdall.urlB}", httpMethod = "POST", body = TextRequest data, headers = [ "Authorization", $"Bearer {heimdall.token}" ])
@@ -37,6 +41,8 @@ let run (area: string) =
         printfn "not changed"
     
     (changed, updated)
+
+
 
 
 mimir.runAll (heimdall.areas, run)
